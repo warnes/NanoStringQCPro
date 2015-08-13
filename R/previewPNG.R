0da7a5c73b1c5f5eb583270b7c40d1ba5ced41aa
r.ziman <r.ziman@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-08-13 23:53:03 +0000

    - Merged recent bugfixes from trunk (mistake in vignette description of   preprocRccSet(); added a check to previewPNG() so that makeQCReport() runs   to completion on inputs that don't have any blank samples).
    - Version 1.0.2.
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_1/madman/Rpacks/NanoStringQCPro@107407 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/previewPNG.R
+++ R/previewPNG.R
@@ -26,36 +26,42 @@
 ##'
 previewPNG <- function(inputFile, outputFile, width, cropHeight, res=72)
 {
-  inputRaster <- readPNG(inputFile)
-
-  rescale_factor <- width / ncol(inputRaster)
-  height <- min( nrow(inputRaster)*rescale_factor, cropHeight )
-
-  croppedInputRaster <- inputRaster[ 1:(height/rescale_factor), 1:(width/rescale_factor), 1:3 ]
+  if (file.exists(inputFile))
+  {
+    inputRaster <- readPNG(inputFile)
   
-  png(filename = outputFile,
-      width = width,
-      height = height,
-      units = "px",
-      res = res)
-
-  op <- par(mar = rep(0,4))
-
-  y_max <- nrow(croppedInputRaster)
-  x_max <- ncol(croppedInputRaster)
-  if (y_max < x_max) {
-    x <- 1:x_max
-    y <- c(1:y_max, rep(y_max, x_max - y_max))
-  } else {
-    x <- c(1:x_max, rep(x_max, y_max - x_max))
-    y <- 1:y_max
-  }  
-  plot( y, x, type = "n", xlab = "", ylab = "", axes = FALSE )
-
-  rasterImage( croppedInputRaster, 1, 1, y_max, x_max )
-
-  par(op)
+    rescale_factor <- width / ncol(inputRaster)
+    height <- min( nrow(inputRaster)*rescale_factor, cropHeight )
   
-  dev.off()
+    croppedInputRaster <- inputRaster[ 1:(height/rescale_factor), 1:(width/rescale_factor), 1:3 ]
+    
+    png(filename = outputFile,
+        width = width,
+        height = height,
+        units = "px",
+        res = res)
+  
+    op <- par(mar = rep(0,4))
+  
+    y_max <- nrow(croppedInputRaster)
+    x_max <- ncol(croppedInputRaster)
+    if (y_max < x_max) {
+      x <- 1:x_max
+      y <- c(1:y_max, rep(y_max, x_max - y_max))
+    } else {
+      x <- c(1:x_max, rep(x_max, y_max - x_max))
+      y <- 1:y_max
+    }  
+    plot( y, x, type = "n", xlab = "", ylab = "", axes = FALSE )
+  
+    rasterImage( croppedInputRaster, 1, 1, y_max, x_max )
+  
+    par(op)
+    
+    dev.off()
+  }
+  else {
+    print("Input file not found")
+  }
 }
 
